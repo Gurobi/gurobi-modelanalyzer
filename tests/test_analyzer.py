b5f7db4a0f304155b4e3495fa8296ee6f10478f6
Richard Oberdieck <oberdieck@oberdieck-macbookpro-2020.local>
2020-08-12 10:41:54 +0200

    First test

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+import os
 
-from model_analyzer.analyzer import
+import model_analyzer.analyzer as analyzer
 
-class TestScenario(TestCase):
+
+class TestAnalyzer(TestCase):
+    def setUp(self) -> None:
+        cwd = os.getcwd()
+
+        self.file_path = os.path.join(cwd, 'dataset', 'glass4.mps')
+
     def test_run(self):
+        data = analyzer.run(self.file_path)
+        print("Done")
